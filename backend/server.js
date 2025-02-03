6fe358eb8348fa8df9c92a82e6c8ecd30bf2df12
krushisutariya <krushisutr@gmail.com>
2025-02-03 19:08:52 +0530

    change in db

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use("", authRoutes);
 app.use("", fileRoutes);
 
-app.listen(3001, () => {
+app.listen(process.env.PORT||3001, () => {
   console.log("Server is running on port 3001");
 });
