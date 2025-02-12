f3ba538644e76fc98a272f76ed04f4a93a7b94c6
krushisutariya <krushisutr@gmail.com>
2025-02-12 13:05:38 +0530

    Deployment issue in  url

--- frontend/dist/assets/javascript-B-iWobVS.js
+++ frontend/dist/assets/javascript-B-iWobVS.js
@@ -1,4 +1,4 @@
-import{conf as t,language as e}from"./typescript-BAdvZ313.js";import"./index-BLElL25i.js";/*!-----------------------------------------------------------------------------
+import{conf as t,language as e}from"./typescript-DrR5xQsV.js";import"./index-DqmFlHD1.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.52.2(404545bded1df6ffa41ea0af4e8ddb219018c6c1)
  * Released under the MIT license
